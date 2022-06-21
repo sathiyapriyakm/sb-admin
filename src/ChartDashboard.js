fe4e1a3f7ca25de6ed9a8442848d3fd1d69bc7e2
sathiyapriyakm <sathiyapriya.km@gmail.com>
2022-06-21 16:18:58 +0530

    Dashboard done

--- src/ChartDashboard.js
+++ src/ChartDashboard.js
@@ -1,5 +1,28 @@
 import React from 'react'
-import {XAxis,YAxis,Tooltip,Area,ResponsiveContainer,AreaChart,CartesianGrid} from "recharts"
+import { LineChart,PieChart,Pie, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+const data= [
+    {Month:"Jan",value:0},
+    {Month:"Feb",value:10000},
+    {Month:"Mar",value:5000},
+    {Month:"Apr",value:15000},
+    {Month:"May",value:10000},
+    {Month:"Jun",value:20000},
+    {Month:"Jul",value:15000},
+    {Month:"Aug",value:25000},
+    {Month:"Sep",value:20000},
+    {Month:"Oct",value:30000},
+    {Month:"Nov",value:25000},
+    {Month:"Dec",value:40000},
+];
+
+const data1=[
+    {Source:"Direct",value:55},
+    {Source:"Referral",value:30},
+    {Source:"Social",value:15},
+]
+
 
 export function ChartDashboard() {
   return (
@@ -26,29 +49,15 @@ export function ChartDashboard() {
         </div>
          <div className="card-body">
             <div className="chart-area"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
-                {/* <canvas id="myAreaChart" style={{display:"block", width: "142px" ,height: "160px"}} width="142" height="160" className="chartjs-render-monitor"></canvas> */}
-           
-                <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+         <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={data} width={500} height={300}>
+        <Line dataKey="value" stroke="#8884d8" />
+          <XAxis dataKey="Month" />
+          <YAxis dataKey="value"/>
           <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
         </LineChart>
-      </ResponsiveContainer>            </div>
+      </ResponsiveContainer>          
+        </div>
         </div>
     </div>
 </div>
@@ -72,33 +81,19 @@ export function ChartDashboard() {
         </div>
         <div className="card-body">
             <div className="chart-pie pt-4 pb-2"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
-                <canvas id="myPieChart" width="142" height="208" style={{display: "block", width: "142px", height: "208px"}} className="chartjs-render-monitor"></canvas>
-                <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
+            <ResponsiveContainer width="100%" height="100%">
+        <PieChart width="100%" height="100%">
+          <Pie data={data1} dataKey="value"  fill="#36b9cc" />
+          <Tooltip/>
+        </PieChart>
       </ResponsiveContainer>
             </div>
             <div className="mt-4 text-center small">
                 <span className="mr-2">
-                    <i className="fas fa-circle text-primary"></i> Direct
+                    <i className="fas fa-circle text-info"></i> Direct
                 </span>
                 <span className="mr-2">
-                    <i className="fas fa-circle text-success"></i> Social
+                    <i className="fas fa-circle text-info"></i> Social
                 </span>
                 <span className="mr-2">
                     <i className="fas fa-circle text-info"></i> Referral
@@ -108,84 +103,10 @@ export function ChartDashboard() {
     </div>
 </div>
 </div>
-    {/* <div className="row">
-<div className="col-xl-8 col-lg-7">
-<div className="card shadow mb-4">
-    <img  src="img/line_chart.png" alt="chart image"/>
-    </div></div>
-    <div className="col-xl-4 col-lg-5">
-    <div className="card shadow mb-4">
-    <img  src="img/pie_chart.png" alt="chart image"/>
-    </div></div>
-    </div> */}
+   
     </>
   );
 }
 
 
-    {/* <div className="card shadow mb-4"> 
-         <div
-            className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-            <div className="dropdown no-arrow">
-                <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                    aria-labelledby="dropdow.ponMenuLink">
-                    <div className="dropdown-header">Dropdown Header:</div>
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>
-        <div className="card-body">
-            <div className="chart-area">
-                <canvas id="myAreaChart"></canvas>
-            </div>
-        </div>
-    </div> 
- </div> 
-
- <div className="col-xl-4 col-lg-5">
-    <div className="card shadow mb-4">
-        <div
-            className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 className="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-            <div className="dropdown no-arrow">
-                <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                    aria-labelledby="dropdownMenuLink">
-                    <div className="dropdown-header">Dropdown Header:</div>
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>
-        <div className="card-body">
-            <div className="chart-pie pt-4 pb-2">
-                <canvas id="myPieChart"></canvas>
-            </div>
-            <div className="mt-4 text-center small">
-                <span className="mr-2">
-                    <i className="fas fa-circle text-primary"></i> Direct
-                </span>
-                <span className="mr-2">
-                    <i className="fas fa-circle text-success"></i> Social
-                </span>
-                <span className="mr-2">
-                    <i className="fas fa-circle text-info"></i> Referral
-                </span>
-            </div>
-        </div>
-    </div>
-</div> */}
-
+   
